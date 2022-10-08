09df09c88a215de6c89bea15048816f4fad8f420
goflutterjava <goflutterjava@gmail.com>
2022-10-08 17:23:54 +0800

    release binary

--- commands/debug/debug_net.go
+++ commands/debug/debug_net.go
@@ -6,8 +6,15 @@ import (
 )
 
 func debugNet(method string, params []string) {
-	if method == "ParseIp" {
+	switch method {
+	case "ParseIp":
 		parseIP := net.ParseIP(params[0])
 		logrus.Info("parse ip is ", parseIP)
+	case "LookupIp":
+		lookupIp, err := net.LookupIP(params[0])
+		if err != nil {
+			panic(err)
+		}
+		logrus.Info("Lookup ip is ", lookupIp)
 	}
 }
